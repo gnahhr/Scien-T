51de5f77f4d98afc1b1cbcc23b3afefa8dade4d4
Matt Domingo <54504555+gnahhr@users.noreply.github.com>
2022-09-09 01:40:39 +0800

    Fixed something
    brought back wrongly pushed codes

--- src/Pages/Mixing Table/mixingTable.jsx
+++ src/Pages/Mixing Table/mixingTable.jsx
@@ -70,8 +70,8 @@ const mixingTable = () => {
     });
   }
 
-  //Check if the selected elements form a compound
-  const mixElems = (elemArr) => {
+   //Check if the selected elements form a compound
+   const mixElems = (elemArr) => {
     let flag = true;
     if (elemArr.length === 0) {
       return;
@@ -86,13 +86,38 @@ const mixingTable = () => {
   	if(mixed.length === 0){
   		alert("No compound of this mixture.");
   	} else {
+      setKnownCompound((knownCompound) => {
+        if (knownCompound.length < 1){
+          return [...knownCompound, ...mixed];
+        } else if (!checkCompounds(mixed, knownCompound)) {
+          return [...knownCompound, ...mixed];
+        } else {
+          return [...knownCompound];
+        }
+      });	
+      
+      //Just show modal if newly discovered
+      if (!checkCompounds(mixed, knownCompound)) {
+        setNewDiscover(mixed);
+        setShowNew(true); 
+      }
+      
       // mixElements(mixed); remain as a comment until further notice - kagagawan ni juicewah
-      setNewDiscover(mixed);
-      setShowNew(true); 		
   	}
+    
     setMixData([]);
   }
 
+  //Check mixData if there are any duplicate elements
+  const checkArray = (symbol, currData) => {
+    return currData.filter((element) => symbol === element).length > 0 ? true : false;
+  }
+
+  //Check knownCompound if there's duplicate known compound
+  const checkCompounds = (mixed, listCompound) => {
+    if (mixed)
+    return listCompound.filter((compound) => mixed[0].name === compound.name).length > 0 ? true : false;
+  }
   
 
   //Compare elements on the mixing table to the recipes list
