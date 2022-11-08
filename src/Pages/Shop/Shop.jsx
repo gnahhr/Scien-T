75d44fc0e70f95c36a0eed37b125abef93ed7c84
Harlequine <ocampojoshua891@gmail.com>
2022-11-08 08:13:00 +0800

    minor tweaks

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,47 +1,60 @@
 import React, { useState } from 'react'
+import jwtDecode from 'jwt-decode'
 
 //Componenst
 import ShopItems from '../../Components/ShopItems'
 
 //Assets
 import MoneyBag from '../../Assets/Images/money-bag.png'
-import Character from '../../Assets/Images/chuu-pewpew.png'
+import Character1 from '../../Assets/Images/chuu-pewpew.png'
 import Arrow from '../../Assets/Images/arrow.svg'
 
 //Style
 import './Shop.css'
+import { useEffect } from 'react'
 
 const Shop = () => {
+    //state
     const [ coins, setCoins ] = useState(999999)
-    const [ username, setUsername ] = useState('michenne')
     const [ selectedItems, setSelectedItems ] = useState(null)
+
+    //
+    const [ username, setUsername ] = useState('')
+    const [ access, setAccess ]  = useState('')
+
+    useEffect(() => {
+    const token = localStorage.getItem('token')
+    const user = jwtDecode(token)
+    setAccess(user.id)
+    setUsername(user.username)
+    },[])
+
 
     return (
         <>
             <div className='main-header'>
                 <h1>Shop</h1>
             </div>
+
             <div className='shop-wrapper'>
+
                 <div className='right'>
                     <div className='user-info'>
                         <div className='upper-part'>
                             <h1>{username}</h1>
                         </div>
+
                         <div className='lower-part'>
                             <img src={MoneyBag} alt="" />
                             <h1>{coins}</h1>
                         </div>
                     </div>
+
                     <div className='character-preview-container'>
-                        <img src={Character} alt='' />
+                        <img src={Character1} alt='' />
                         <div className='circular-platform'></div>
-                        <div className='arrow-carousel-left'>
-                            <img src={Arrow} alt="" />
-                        </div>
-                        <div className='arrow-carousel-right'>
-                            <img src={Arrow} alt="" />
-                        </div>
                     </div>
+
                 </div>
                 <div className='left'>
                     <ShopItems selectedItems={setSelectedItems}/>
