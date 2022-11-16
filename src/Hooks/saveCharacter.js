ea1d28d4ac407e11574d9b4b399beaf0a71b4465
Harlequine <ocampojoshua891@gmail.com>
2022-11-16 14:25:55 +0800

    minor tweaks

--- src/Hooks/saveCharacter.js
+++ src/Hooks/saveCharacter.js
@@ -1,11 +1,11 @@
-export default async function saveCharacter(access, gender, accessories, base64, base64hit){
+export default async function saveCharacter(access, gender, top, bottom, accessory, base64, base64hit){
     const response = await fetch('/api/saveCharacter/' + access + '/' + gender,{
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            base64,base64hit,accessories
+            base64, base64hit, top, bottom, accessory
         })
     })
 
