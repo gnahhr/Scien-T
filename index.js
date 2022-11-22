2cf43fb04735e3412fb904181ae568a3b03960ee
Matt Domingo <54504555+gnahhr@users.noreply.github.com>
2022-11-22 21:33:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const cors = require ('cors')
 const bodyParser = require ('body-parser')
 const mongoose = require ('mongoose')
-const path = require ('path')
+// const path = require ('path')
 
 require('dotenv/config')
 
@@ -27,14 +27,14 @@ mongoose.connect(process.env.DB_URI, {useNewUrlParser:true, useUnifiedTopology:t
 const PORT = process.env.PORT || 6969; // backend routing port
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//     // Set static folder
+//     app.use(express.static('client/build'));
   
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+//   }
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
